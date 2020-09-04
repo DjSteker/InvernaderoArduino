5cd3ec5e30e775950ac79bba2fb88ccc401f3a10
DjSteker <37488993+DjSteker@users.noreply.github.com>
2020-09-04 17:31:36 +0200

    Create classDatos.h

---
+++ ServidorAlfa001/classDatos.h
@@ -0,0 +1,28 @@
+
+class classDatos {
+
+  public:
+    double HumedadAmbiente = 0;
+    double TemperaturaAmbiente = 0 ;
+    double PresionAmbiente = 0;
+    double HumedadSuelo1 = 0 ;
+    double HumedadSuelo2 = 0 ;
+    int Anio = 0 ;
+    int Mes = 0 ;
+    int Dia = 0 ;
+    int Hora = 0 ;
+    int Minuto = 0 ;
+    int Segundo = 0 ;
+    int Riego1Segundos = 0 ;
+    int Riego2Segundos = 0 ;
+
+    float PresionHoras[5];
+    float PresionDias[5];
+    float TemperaturaHoras[5];
+    float TemperaturaDias[5];
+
+    byte PrediccionPresion;
+  private:
+
+
+};
