feac411a0a4f7629257306763a675fef22ce89e0
DjSteker <37488993+DjSteker@users.noreply.github.com>
2020-09-08 00:12:43 +0200

    Update Servidor.h

--- ServidorAlfa001/Servidor.h
+++ ServidorAlfa001/Servidor.h
@@ -21,13 +21,46 @@ void handleForm();
 void handle_OnConnect();
 void handle_SD();
 void handle_geoposicion();
-void handle_Historial();
-String SendHTML(float temperature, float humidity, float pressure, float altitude, String FechaHtml);
+void handle_HistorialHoras();
+void handle_HistorialDias();
+void handle_Fecha();
+
+String SendHTML(float temperature, float humidity, float pressure, float altitude, String FechaHtml, float PrediccionPresion);
 String SendHTML_SD();
 void SetParametrosHtml(float temperature_var, float humidity_var, float pressure_var, float Tempetura_var[5], float Presion_var[5], String FechaHtml_var);
 void SetParametrosHtmlSD(String Texto);
-void SetParametrosHtmlPrediccionBaro(byte Pred_var);
+//void SetParametrosHtmlPrediccionBaro(float Pred_var);
 
 
 int Riego1ServerGet();
 int Riego2ServerGet();
+
+bool ServerNuevaFecha();
+//ESP8266WebServer server(int a);
+
+//class server : public ESP8266WebServer {
+//  public:
+//    server(int a);
+//
+//  private:
+//    int32_t _id;
+//
+//};
+
+
+//class server : public ESP8266WebServer {
+//protected:
+//
+
+//class server : public ESP8266WebServer{
+//  public:
+//    server(int puerto);
+//    void loopServer() ;
+//    void setup_Inicio();
+//
+//  private:
+//    void handleRoot() ;
+//    void handlePlain() ;
+//    void handleForm() ;
+//    void handleNotFound() ;
+//};
