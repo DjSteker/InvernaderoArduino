210fa1987741edf546851ffb00621b60e64a44c1
DjSteker <37488993+DjSteker@users.noreply.github.com>
2020-09-13 20:42:47 +0200

    Update Memoria.h

--- ServidorAlfa001/Memoria.h
+++ ServidorAlfa001/Memoria.h
@@ -1,12 +1,17 @@
 
-
 void Guardar_Hora(int Hora,  double HumedadAmbiente,  double TemperaturaAmbiente, double PresionAmbiente, double HumedadSuelo1, double HumedadSuelo2) ;
-
-void Guardar_Dia(int Anio, int Mes, int Dia, int Hora, int Minuto, int Segundo , double HumedadAmbiente,  double TemperaturaAmbiente, double PresionAmbiente, double HumedadSuelo1, double HumedadSuelo2) ;
+//
+//void Guardar_Dia(int Anio, int Mes, int Dia, int Hora, int Minuto, int Segundo , double HumedadAmbiente,  double TemperaturaAmbiente, double PresionAmbiente, double HumedadSuelo1, double HumedadSuelo2) ;
+void Guardar_Dia(int Anio, int Mes, int Dia, int Hora, int Minuto, int Segundo ) ;
 
 String LeerArchivo() ;
+
 
 
 String* LeerValores() ;
 
 bool ExisteArchivo();
+
+String LeerValoresHtml();
+
+String LeerValoresRestauracion();
