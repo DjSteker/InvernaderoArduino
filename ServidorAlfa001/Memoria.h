bf35da2d93d172e95edc8b65d81951d69f92a0de
DjSteker <37488993+DjSteker@users.noreply.github.com>
2020-09-24 20:47:54 +0200

    Update Memoria.h

--- ServidorAlfa001/Memoria.h
+++ ServidorAlfa001/Memoria.h
@@ -1,4 +1,6 @@
 
+
+
 void Guardar_Hora(int Hora,  double HumedadAmbiente,  double TemperaturaAmbiente, double PresionAmbiente, double HumedadSuelo1, double HumedadSuelo2) ;
 //
 //void Guardar_Dia(int Anio, int Mes, int Dia, int Hora, int Minuto, int Segundo , double HumedadAmbiente,  double TemperaturaAmbiente, double PresionAmbiente, double HumedadSuelo1, double HumedadSuelo2) ;
@@ -19,5 +21,6 @@ long ConvierteFechaEnNumero( int year, int mes, int dia);
 void SetMemoriaDias(String Linea, int DiaIdice);
 
 void SetMemoriaHoras(String Linea);
+void SetMemoriaHoraParametros(String Registro, int HoraIndice);
 
 //String ProcesarLineaRestauracion ( String Linea_var );
